2a50f980dbfdb87733da8681406c77c5f69787be
sreekanthr13 <38903062+sreekanthr13@users.noreply.github.com>
2024-01-13 13:46:45 +0530

    updated column cfee datatype

--- TechBoxtoUnbox/Models/DTO/Course.cs
+++ TechBoxtoUnbox/Models/DTO/Course.cs
@@ -9,7 +9,7 @@ namespace TechBoxtoUnbox.Models.DTO
         
         public string CName { get; set; }
         
-        public decimal CFee { get; set; }
+        public double CFee { get; set; }
         public DateTime Createddate { get; set; }
     }
 }
