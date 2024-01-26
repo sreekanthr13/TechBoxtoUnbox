f9961724ea84d9144f82dbe76e49b36a576695e9
sreekanthr13 <38903062+sreekanthr13@users.noreply.github.com>
2024-01-26 23:59:42 +0530

    Only my Changes

--- TechBoxtoUnbox/Migrations/20240126180336_Created realtion studentCourses table.Designer.cs
+++ TechBoxtoUnbox/Migrations/20240126180336_Created realtion studentCourses table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechBoxtoUnbox.DataAccess;
 
@@ -11,9 +12,11 @@ using TechBoxtoUnbox.DataAccess;
 namespace TechBoxtoUnbox.Migrations
 {
     [DbContext(typeof(CourseContext))]
-    partial class CourseContextModelSnapshot : ModelSnapshot
+    [Migration("20240126180336_Created realtion studentCourses table")]
+    partial class CreatedrealtionstudentCoursestable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TechBoxtoUnbox/Migrations/20240126180336_Created realtion studentCourses table.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechBoxtoUnbox.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedrealtionstudentCoursestable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.CreateTable(
+                name: "StudentCourses",
+                columns: table => new
+                {
+                    StdCrs_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Student_Id = table.Column<int>(type: "int", nullable: false),
+                    Course_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentCourses", x => x.StdCrs_Id);
+                    table.ForeignKey(
+                        name: "FK_StudentCourses_Courses_Course_id",
+                        column: x => x.Course_id,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentCourses_Students_Student_Id",
+                        column: x => x.Student_Id,
+                        principalTable: "Students",
+                        principalColumn: "Student_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentCourses_Course_id",
+                table: "StudentCourses",
+                column: "Course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentCourses_Student_Id",
+                table: "StudentCourses",
+                column: "Student_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentCourses");
+        }
+    }
+}
