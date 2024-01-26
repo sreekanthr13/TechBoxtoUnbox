6195b473e86ed4e4df8f20cf8600339627b5332c
sreekanthr13 <38903062+sreekanthr13@users.noreply.github.com>
2024-01-26 23:41:32 +0530

    Created StudentCourses table

--- TechBoxtoUnbox/Models/DTO/Course.cs
+++ TechBoxtoUnbox/Models/DTO/Course.cs
@@ -11,5 +11,7 @@ namespace TechBoxtoUnbox.Models.DTO
         
         public double CFee { get; set; }
         public DateTime Createddate { get; set; }
+
+        public ICollection<StudentCourses> Studentcourses { get; set; }
     }
 }
